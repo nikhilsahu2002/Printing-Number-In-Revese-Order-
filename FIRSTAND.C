e3cd529bff135b9ea580ffe5daf9c5d8abbd0dbe
nikhilsahu2002 <117375980+nikhilsahu2002@users.noreply.github.com>
2022-12-01 19:23:16 +0530

    Update FIRSTAND.C

--- FIRSTAND.C
+++ FIRSTAND.C
@@ -2,17 +2,17 @@
 #include<conio.h>
 void main()
 {
-    int num, rem, prod = 1;
+    long long num, rem, prod = 1;
     clrscr();
     printf("Enter a number: ");
-    scanf("%d", &num);
+    scanf("%lld", &num);
     printf("All Number In Reverse Order");
     while(num != 0)
     {
 	rem = num % 10;
 	prod = rem;
 	num /=  10;
-	printf(" %d",prod);
+	printf(" %lld",prod);
     }
 
 getch();
